4f89e474a7d2ae37efe09cdccb1bccb44a902295
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2020-08-11 21:25:09 +0530

    CoronaPlotter Done

---
+++ index.js
@@ -0,0 +1,31 @@
+// Funtion to update the map
+function updateMap() {
+  //fetchApi
+  console.log("Updating the Map");
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((rsp) => {
+      //   console.log(rsp.data);
+
+      rsp.data.forEach((element) => {
+        latitude = element.latitude;
+        longitude = element.longitude;
+
+        cases = element.infected;
+        if (cases > 255) {
+          color = "rgb(255,0,0)";
+        } else {
+          color = `rgb(${cases}, 0, 0)`;
+        }
+        //Mark on the map
+        new mapboxgl.Marker({
+          draggable: false,
+          color: color,
+        })
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+      });
+    }); 
+}
+let interval = 20000;
+setInterval(updateMap, interval);
